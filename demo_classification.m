e61d4c0ee8d9cf75fbe4cda5b4cafbce00b03b37
Jing Wang <yuzhounh@163.com>
2017-04-20 22:56:38 +0800

    Update demo_classification.m

--- demo_classification.m
+++ demo_classification.m
@@ -1,5 +1,5 @@
 % Classify on AR data with natural occlusions, using the unoccluded images
-% as training set and the occluded images as testing set
+% as training set and the occluded images as testing set.
 
 %     2DPCA with L1-norm for simultaneously robust and sparse modelling
 %     Copyright (C) 2013 Jing Wang
